92a49bb49ca12024be68e18f456bd9eb860f80cd
Luciano Melo <lucianohsmelo@gmail.com>
2021-03-27 11:01:56 -0300

    Add routes

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <h1>UI Home</h1>
+  )
+}
+
+const Manager = () => {
+  return (
+    <h1>UI Manager</h1>
+  )
+}
+
+const PageNotFound = () => {
+  return (
+    <h1>Page Not Found</h1>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="view-routes">
+      <Switch>
+        <Route path="/ui" exact={true} component={Home} />
+        <Route path="/ui/manager" component={Manager} />
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
   );
 }
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import path from 'path'
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../', 'react-app', 'build')));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../', 'react-app', 'build', 'index.html')));
 
-
+app.get('/api', (req, res) => res.send('Api'))
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../', 'react-app', 'build', 'index.html')));
 
 app.listen(3000, () => console.log('App Listen'))
